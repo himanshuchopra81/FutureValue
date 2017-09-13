b11be3af985619d9df58be4aee1474e5e1a80905
691062 <691062@01-04-02-01>
2017-09-13 13:13:42 -0400

    Add project files.

---
+++ FutureValue/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FutureValue.Startup))]
+namespace FutureValue
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
